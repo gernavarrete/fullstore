571974c6f2789327998360edb099034ed64c5709
gernavarrete <germannavarrete.info@gmail.com>
2023-10-16 22:34:27 -0300

    module js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,28 +1,51 @@
-async function getApi() {
-  let products = await fetch("https://dummyjson.com/products?limit=9")
+export async function getApi(url, containerId) {
+  let products = await fetch(url)
     .then((res) => res.json())
-    .then((data) => data.products);
-  console.table(products);
-  let productsElements = products.map(
-    (product) =>
-      `<div class="divCard"><h1>${product.title}</h1>
-  <div class="divImages">
-  <span class="material-symbols-outlined">
-  arrow_back_ios
-  </span>
-  <img src="${product.images[0]}" alt="${product.title}">
+    .then((data) =>
+      data.products
+        .map(
+          (product) =>
+            `<div class="divCard"><h1>${product.title}</h1>
+    <div class="divImages">
     <span class="material-symbols-outlined">
-    arrow_forward_ios
+    arrow_back_ios
     </span>
-  </div>
-    <h1>u$s ${product.price}</h1>
-    <button>Add Cart</button>
-  </div>`
-  );
-  console.log(productsElements.toString());
-  document.getElementById("productSection").innerHTML = productsElements
-    .toString()
-    .replaceAll(",", "");
+    <img src="${product.images[0]}" alt="${product.title}">
+      <span class="material-symbols-outlined">
+      arrow_forward_ios
+      </span>
+    </div>
+      <h1>u$s ${product.price}</h1>
+      <button>Add Cart</button>
+    </div>`
+        )
+        .toString()
+        .replaceAll(",", "")
+    );
+  console.log(products);
+
+  //document.getElementById(containerId).innerHTML = products
 }
 
-getApi();
+export class Products {
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+  }
+
+  fetchData(url, cb, options = {}) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (!cb) {
+          console.log(data);
+          return;
+        }
+
+        let products = cb(data);
+        this.#renderContainer(products);
+      });
+  }
+  #renderContainer(data) {
+    this.container.innerHTML = data;
+  }
+}
---
+++ assets/js/renderProducts.js
@@ -0,0 +1,28 @@
+export function renderProducts(data) {
+  let products = "";
+  if (!data) {
+    console.log("Data Not Found");
+    return null;
+  }
+  let dataProducts = data.products;
+  products = dataProducts
+    .map(
+      (product) => `<div class="divCard"><h1>${product.title}</h1>
+            <div class="divImages">
+            <span class="material-symbols-outlined">
+            arrow_back_ios
+            </span>
+            <img src="${product.images[0]}" alt="${product.title}">
+              <span class="material-symbols-outlined">
+              arrow_forward_ios
+              </span>
+            </div>
+              <h1>u$s ${product.price}</h1>
+              <button>Add Cart</button>
+            </div>`
+    )
+    .toString()
+    .replaceAll(",", "");
+
+  return products;
+}
